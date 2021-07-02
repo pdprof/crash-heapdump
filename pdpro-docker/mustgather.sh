1a84334741a1363672dd4dbcdb3530c706fc4725
Kenichi Ooi <kenoi@jp.ibm.com>
2021-07-02 21:42:45 +0900

    copy /logs/* files to /pdprof/dump after dump

--- pdpro-docker/mustgather.sh
+++ pdpro-docker/mustgather.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+if [ ! -d /pdprof/dump ]; then
+	mkdir -p /pdprof/dump
+fi
+
 cd /pdprof/dump
 /config/linperf.sh 1
 SVCMD=`which server`
 if [ -f $SVCMD ]; then
     $SVCMD dump defaultServer --archive=/pdprof/dump/defaultServer.dump.zip
 fi
+cp -R -p /logs/* /pdprof/dump
